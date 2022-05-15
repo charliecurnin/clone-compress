52eec6d5d6f3308546e409d94a88cea5acc60643
Charlie Curnin <charliecurnin@Charlies-MacBook-Pro-2.local>
2022-05-15 15:24:22 -0700

    preprocess: remove silence

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
+import librosa
+import numpy as np
+
 def preprocess(wav):
   """
   Preprocess a single audio file:
   - Remove pauses
   """
+
+  # Remove pauses
+  """
+  Borrowed from code I wrote for CS 224S HW1
+  """
+
+  speech_intervals = librosa.effects.split(wav)
+  wav = np.concatenate([
+    wav[start_i:end_i] for (start_i, end_i_ in speech_intervals
+  ])
 
   return wav
