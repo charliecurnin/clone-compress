0ae7eade80f1cf7eab7b3463eeee5f15426fcb7c
Charlie Curnin <charliecurnin@Charlies-MacBook-Pro-2.local>
2022-05-15 15:59:19 -0700

    add compress skeleton

---
+++ compress.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def compress(wav):
+  """
+  Compress a single audio file:
+
+  Input: audio preprocessed by `preprocess`
+  Output: 
+  - A clip of the original audio
+  - A transcript
+  """
+
+  clip = None
+  transcript = None
+
+  return clip, transcript
