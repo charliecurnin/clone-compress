c638a5a7c54048a8b7fd912fd523eb7ad63ae93b
Charlie Curnin <charliecurnin@Charlies-MacBook-Pro-2.local>
2022-05-15 18:00:51 -0700

    extract clip

--- compress.py
+++ compress.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
-def compress(wav):
+def get_clip(wav_path):
+  
+  audio_ex, sr = librosa.load(agent_audio_file)
+  
+  # Naive clipping method: Take first 10 seconds
+  clip_duration_seconds = min(10, sr * len(audio_ex))
+  n_frames = clip_duration_seconds * sr
+  audio_clip = audio_ex[:n_frames]
+  return audio_clip
+
+def get_transcript(wav_path):
+  return ""
+
+def compress(wav_path):
   """
   Compress a single audio file:
 
-  Input: audio preprocessed by `preprocess`
+  Input:
+  - audio, preprocessed by `preprocess`
   Output: 
   - A clip of the original audio
   - A transcript
   """
 
-  clip = None
-  transcript = None
+  clip = get_clip(wav_path)
+  transcript = get_transcript(wav_path)
 
   return clip, transcript
